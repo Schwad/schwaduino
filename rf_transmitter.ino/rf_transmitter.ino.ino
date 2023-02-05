8a035314af3bd23deb8cd6afc97f6abc1047cae7
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2023-02-05 19:01:03 +0000

    goooo

--- rf_transmitter.ino/rf_transmitter.ino.ino
+++ rf_transmitter.ino/rf_transmitter.ino.ino
@@ -1,4 +1,5 @@
 // RF Transmission libraries
+// Mirf docs http://archive.communica.co.za/Content/Catalog/Documents/D2018958833.pdf
 #include <SPI.h>
 #include <Mirf.h>
 #include <nRF24L01.h>
@@ -6,19 +7,42 @@
 
 //  Library to make keypresses more elegant
 #include <Bounce2.h>
-#define BUTTON_PIN 8 // D8 nano pin
-Bounce2::Button button = Bounce2::Button(); // Docs https://github.com/thomasfredericks/Bounce2
+#define BUTTON_PIN_ONE 8 // D8 nano pin
+#define BUTTON_PIN_TWO 7 // D7 nano pin
+#define BUTTON_PIN_THREE 6 // D6 nano pin
+#define TRANSMISSION_BUTTON_PIN 5 // D5 nano pin
+#define CANCEL_BUTTON_PIN 4 // D4 nano pin
+// Docs https://github.com/thomasfredericks/Bounce2
+Bounce2::Button button_one = Bounce2::Button();
+Bounce2::Button button_two = Bounce2::Button();
+Bounce2::Button button_three = Bounce2::Button();
+Bounce2::Button transmission_button = Bounce2::Button();
+Bounce2::Button cancel_button = Bounce2::Button();
 
 void setup()
 {
   Serial.begin(9600);
 
   // All button setup
-  button.attach(BUTTON_PIN,INPUT_PULLUP); // Attach the debouncer to a pin with INPUT_PULLUP mode, if this doesn't work just do pullup
+  // Attach the debouncer to a pin with INPUT_PULLUP mode, if this doesn't work just do pullup
+  button_one.attach(BUTTON_PIN_ONE,INPUT_PULLUP); 
+  button_two.attach(BUTTON_PIN_TWO,INPUT_PULLUP); 
+  button_three.attach(BUTTON_PIN_THREE,INPUT_PULLUP); 
+  transmission_button.attach(TRANSMISSION_BUTTON_PIN,INPUT_PULLUP); 
+  cancel_button.attach(CANCEL_BUTTON_PIN,INPUT_PULLUP); 
+
   // DEBOUNCE INTERVAL IN MILLISECONDS
-  button.interval(5); 
+  button_one.interval(5); 
+  button_two.interval(5); 
+  button_three.interval(5); 
+  transmission_button.interval(5); 
+  cancel_button.interval(5); 
   // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
-  button.setPressedState(LOW); 
+  button_one.setPressedState(LOW); 
+  button_two.setPressedState(LOW); 
+  button_three.setPressedState(LOW); 
+  transmission_button.setPressedState(LOW); 
+  cancel_button.setPressedState(LOW); 
   
   // All Transmitter Setup
   Mirf.cePin = 9; //Set the CE pin to D9 
@@ -44,19 +68,48 @@ void setup()
 unsigned int adata = 0; 
 
 //Global for number of presses
-unsigned int number_of_presses = 0;
+unsigned int one_number_of_presses = 0;
+unsigned int two_number_of_presses = 0;
+unsigned int three_number_of_presses = 0;
 
 void loop()
 {
   // button handler
   button_one.update();
+  button_two.update();
+  button_three.update();
 
-  if ( button.pressed() ) {
+  if ( button_one.pressed() ) {
+    Serial.println("One pressed");
     one_number_of_presses++;
     two_number_of_presses = 0;
     three_number_of_presses = 0;
     Serial.println(one_number_of_presses);
     
+  }
+
+  if ( button_two.pressed() ) {
+    Serial.println("Two pressed");
+    one_number_of_presses = 0;
+    two_number_of_presses++;
+    three_number_of_presses = 0;
+    Serial.println(two_number_of_presses);
+    
+  }
+
+  if ( button_three.pressed() ) {
+    Serial.println("Three pressed");
+    one_number_of_presses = 0;
+    two_number_of_presses = 0;
+    three_number_of_presses++;
+    Serial.println(three_number_of_presses);
+    
+  }
+
+  if ( cancel_button.pressed() ) {
+    one_number_of_presses = 0;
+    two_number_of_presses = 0;
+    three_number_of_presses = 0;
   }
 
   if ( transmission_button.pressed() ) {
@@ -70,6 +123,34 @@ void loop()
           break;
         case 2:
           Serial.println("goin to the pub");
+          break;
+      }
+    }
+
+    if(two_number_of_presses > 0) {
+      switch(two_number_of_presses % 3) {
+        case 0:
+          Serial.println("I love you");
+          break;
+        case 1:
+          Serial.println("You're the best");
+          break;
+        case 2:
+          Serial.println("Bed for manatee?");
+          break;
+      }
+    }
+
+    if(three_number_of_presses > 0) {
+      switch(three_number_of_presses % 3) {
+        case 0:
+          Serial.println("Yes");
+          break;
+        case 1:
+          Serial.println("No");
+          break;
+        case 2:
+          Serial.println("Maybe");
           break;
       }
     }
